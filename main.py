3703a03d7b141e1cd948a54f308ab60152358b1d
ZachPinet4 <zekep04@gmail.com>
2023-03-09 13:11:32 -0500

    Commit pls

--- main.py
+++ main.py
@@ -1,26 +1,40 @@
 # This is the encode function.
-def encode():
+def encode(password):
+    encoded_password = ''
+    for x in str(password):
+        x = int(x)
+        x += 3
+        encoded_password += str(x)
+
+    print("Your password has been encoded and stored!")
+    return int(encoded_password)
+
+
+# This is the decode function.
+def decode(encoded_password):
     pass
-
-
-# Change
-def func(x):
-    print(x)
 
 
 # This is the main function for the program.
 def main():
-    try:
-        num = int(input("Enter a number: "))
-        if num < 0:
-            raise ValueError("Wrong number!")
+    while True:
+        print("Menu\n"
+              "-------------\n"
+              "1. Encode\n"
+              "2. Decode\n"
+              "3. Quit")
 
-    except ValueError as problem:
-        print(problem)
+        menu_option = int(input("Please enter an option: "))
+        encoded_password = ''
 
-    i_sum = 0
-    for i in range(num):
-        i_sum += 1
+        if menu_option == 1:
+            password = int(input("Please enter your password to encode: "))
+            encoded_password = encode(password)
+        elif menu_option == 2:
+            password = decode(encoded_password)
+            print(f"The encoded password is {encoded_password}, and the original password is {password}".)
+        else:
+            break
 
 
 # This starts the program by calling the main function.
